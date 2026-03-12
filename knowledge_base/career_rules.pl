b3636d80e2f5e28fe679a99d0f7d8bf1884aa059
Keys <keys14052002@gmail.com>
2026-03-12 11:00:11 +0000

    feat: add data analyst and ui/ux designer to the knowledge base

--- knowledge_base/career_rules.pl
+++ knowledge_base/career_rules.pl
@@ -3,3 +3,13 @@
 career(software_engineer) :-
     prerequisite(programming, high),
     prerequisite(interest, coding).
+
+career(data_analyst) :-
+    prerequisite(statistics, high),
+    prerequisite(mathematics, high),
+    prerequisite(interest, data).
+
+career(ui_ux_designer) :-
+    prerequisite(design, high),
+    prerequisite(creativity, high),
+    prerequisite(interest, design).
